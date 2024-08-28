1c5c0cea22d187c237c5653cd3d0b63282900b09
ToastedToast <hey@toasted.dev>
2024-08-28 17:58:01 +0800

    feat: show "sent an attachment" message when an attachment is sent

--- src/main/java/dev/toasted/discordo/DiscordBot.java
+++ src/main/java/dev/toasted/discordo/DiscordBot.java
@@ -3,6 +3,8 @@ package dev.toasted.discordo;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -13,15 +15,28 @@ public class DiscordBot {
 
     public void serverTick(MinecraftServer server) {
         if(this.hasReceivedMessage) {
-            Text message = Text.literal("[Discord] ").formatted(Formatting.DARK_BLUE)
-                .append(
-                    Text.literal(this.messageReceivedEvent.getMember().getEffectiveName())
-                        .setStyle(Style.EMPTY.withColor(this.messageReceivedEvent.getMember().getColorRaw()))
-                )
-                .append(
-                    Text.literal(": " + this.messageReceivedEvent.getMessage().getContentRaw())
-                        .formatted(Formatting.WHITE)
-                );
+            MutableText message = Text.literal("[Discord] ").formatted(Formatting.BLUE)
+                    .append(
+                        Text.literal(this.messageReceivedEvent.getMember().getEffectiveName())
+                            .setStyle(Style.EMPTY.withColor(this.messageReceivedEvent.getMember().getColorRaw()))
+                    )
+                    .append(
+                        Text.literal(": ").formatted(Formatting.WHITE)
+                    );
+            if(this.messageReceivedEvent.getMessage().getContentRaw().isEmpty() &&
+                    !this.messageReceivedEvent.getMessage().getAttachments().isEmpty()) {
+                message
+                    .append(
+                        Text.literal("sent an attachment")
+                            .formatted(Formatting.WHITE, Formatting.ITALIC)
+                    );
+            } else {
+                message
+                    .append(
+                        Text.literal(this.messageReceivedEvent.getMessage().getContentRaw())
+                            .formatted(Formatting.WHITE)
+                    );
+            }
             for(ServerPlayerEntity player: server.getPlayerManager().getPlayerList()) {
                 player.sendMessage(message, false);
             }
