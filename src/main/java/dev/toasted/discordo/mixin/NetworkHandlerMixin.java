259d6673b574c466f69d86f50b0ec1e624b769f1
ToastedToast <hey@toasted.dev>
2024-08-28 21:00:38 +0800

    feat: add join and leave messages

---
+++ src/main/java/dev/toasted/discordo/mixin/NetworkHandlerMixin.java
@@ -0,0 +1,39 @@
+package dev.toasted.discordo.mixin;
+
+import dev.toasted.discordo.Constants;
+import dev.toasted.discordo.Discordo;
+import net.minecraft.network.DisconnectionInfo;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class NetworkHandlerMixin {
+    @Shadow
+    public ServerPlayerEntity player;
+
+    @Inject(at = @At(value = "HEAD"), method = "onDisconnected")
+    private void onPlayerLeave(DisconnectionInfo info, CallbackInfo ci) {
+        if(Discordo.INSTANCE == null) return;
+
+        String message = "⬅️ " + player.getName().getString() + " has left the game";
+
+        if(Discordo.INSTANCE.config.webhookEnabled) {
+            Discordo.INSTANCE.webhook
+                .sendMessage(message)
+                .setUsername(player.getName().getString())
+                .setAvatarUrl("https://crafthead.net/avatar/" + player.getUuidAsString())
+                .setAllowedMentions(Constants.AllowedMentions)
+                .queue();
+        } else {
+            Discordo.INSTANCE.channel
+                .sendMessage(message)
+                .setAllowedMentions(Constants.AllowedMentions)
+                .queue();
+        }
+    }
+}
---
+++ src/main/java/dev/toasted/discordo/mixin/PlayerManagerMixin.java
@@ -0,0 +1,36 @@
+package dev.toasted.discordo.mixin;
+
+import dev.toasted.discordo.Constants;
+import dev.toasted.discordo.Discordo;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ConnectedClientData;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerManager.class)
+public class PlayerManagerMixin {
+    @Inject(at = @At(value = "TAIL"), method = "onPlayerConnect")
+    private void onPlayerJoin(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo info) {
+        if(Discordo.INSTANCE == null) return;
+
+        String message = "➡️ " + player.getName().getString() + " has joined the game";
+
+        if(Discordo.INSTANCE.config.webhookEnabled) {
+            Discordo.INSTANCE.webhook
+                .sendMessage(message)
+                .setUsername(player.getName().getString())
+                .setAvatarUrl("https://crafthead.net/avatar/" + player.getUuidAsString())
+                .setAllowedMentions(Constants.AllowedMentions)
+                .queue();
+        } else {
+            Discordo.INSTANCE.channel
+                .sendMessage(message)
+                .setAllowedMentions(Constants.AllowedMentions)
+                .queue();
+        }
+    }
+}
